cf389b208e500c964f42e572dd31001eec665b9a
Freddie <fpokrzywa@gmail.com>
2025-08-06 21:58:13 +0000

    the current width of the page is not 100% wide

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -209,7 +209,7 @@ export function Dashboard() {
             />
           </ScrollArea>
           <div className="p-4 bg-transparent w-full flex justify-center">
-            <div className="w-full max-w-4xl">
+            <div className="w-full">
               <ChatInput onSubmit={handleCreateWidget} onSave={handleSaveQuery} loading={loading} />
             </div>
           </div>
