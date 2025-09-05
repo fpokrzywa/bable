c2c2e1ef0d2458d1a49d088e16dfdb6103d3a646
Freddie Pokrzywa <freddie@3cpublish.com>
2025-09-05 17:34:17 -0400

    Bable_web

--- src/ai/genkit.ts
+++
@@ -1,7 +0,0 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
-
-export const ai = genkit({
-  plugins: [googleAI({apiKey: 'AIzaSyAytIneVxy8TLyFcFd0pZdnz8P5lV9iMNc'})],
-  model: 'googleai/gemini-2.0-flash',
-});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 
 import type {Metadata} from 'next';
 import './globals.css';
-import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
   title: 'BabelPhish',
@@ -25,7 +24,6 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         {children}
-        <Toaster />
       </body>
     </html>
   );
